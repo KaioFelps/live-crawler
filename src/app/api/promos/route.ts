381a2a12f7d1d25c698d0758c2dd81921b3350ca
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-07-24 16:09:08 -0300

    build: prepare api route for production

--- src/app/api/promos/route.ts
+++ src/app/api/promos/route.ts
@@ -1,60 +1,73 @@
+import "dotenv/config"
 import { NextResponse } from "next/server"
 import puppeteer from "puppeteer"
 
 export type ArticleObject = {
-    title: string;
-    link: string;
-    deadLine: string;
-    gender: string;
-    goal: string;
-    cover: string;
-    badge: string;
+  title: string;
+  link: string;
+  deadLine: string;
+  gender: string;
+  goal: string;
+  cover: string;
+  badge: string;
+}
+
+async function getBrowser() {
+  const environment = process.env.NODE_ENV
+
+  let browser;
+
+  if(environment === "production") {
+    browser = await puppeteer.connect({browserWSEndpoint: `wss://chrome.browserless.io?token=${process.env.BROWSERLESS_API_KEY}`})
+  } else {    
+    browser = await puppeteer.launch()
   }
 
-export async function GET(req: Request) {
-    try {
-        const browser = await puppeteer.launch({
-          args: ["--no-sandbox"]
-        })
-          const page = await browser.newPage()
-    
-          await page.goto("https://habblive.in/noticias/184", {
-            waitUntil: "domcontentloaded",
-            timeout: 7000
-          })
-    
-          await page.waitForSelector("[style='margin: 0px; padding-bottom: 1em;']")
-    
-          const promosObjects = await page.evaluate(() => {
-            const promos = document.querySelectorAll("[style='margin: 0px; padding-bottom: 1em;']")
-    
-            const formattedPromos: Array<ArticleObject> = []
-            
-            promos.forEach((promo) => {
-              const formattedPromo = {
-                title: promo.querySelector("a")!.textContent!,
-                link: promo.querySelector("a")!.href,
-                deadLine: (promo.querySelector("b + br")!.previousSibling! as any).data,
-                gender: (promo.querySelector("br + b + br + b")!.nextSibling! as any).data,
-                goal: (promo.querySelector("br + b + br + b + br + b")!.nextSibling! as any).data,
-                cover: (promo.querySelector("br")!.previousSibling! as HTMLImageElement).src,
-                badge: (promo.querySelector("br")!.previousSibling!.previousSibling as HTMLImageElement)!.src,
-              }
-    
-              formattedPromos.push(formattedPromo)
-            })
-    
-            return formattedPromos
-          })
-    
-        await browser.close()
+  return browser
+}
 
-        return NextResponse.json({data: promosObjects})
-      } catch (error) {
-        console.log(error)
-    
-        return NextResponse.json({data: []});
-      }  
+export async function GET(req: Request) {
+  try {
+    const browser = await getBrowser()
+      const page = await browser.newPage()
+
+      await page.goto("https://habblive.in/noticias/184", {
+        waitUntil: "domcontentloaded",
+        timeout: 7000
+      })
+
+      await page.waitForSelector("[style='margin: 0px; padding-bottom: 1em;']")
+
+      const promosObjects = await page.evaluate(() => {
+        const promos = document.querySelectorAll("[style='margin: 0px; padding-bottom: 1em;']")
+
+        const formattedPromos: Array<ArticleObject> = []
+        
+        promos.forEach((promo) => {
+          const formattedPromo = {
+            title: promo.querySelector("a")!.textContent!,
+            link: promo.querySelector("a")!.href,
+            deadLine: (promo.querySelector("b + br")!.previousSibling! as any).data,
+            gender: (promo.querySelector("br + b + br + b")!.nextSibling! as any).data,
+            goal: (promo.querySelector("br + b + br + b + br + b")!.nextSibling! as any).data,
+            cover: (promo.querySelector("br")!.previousSibling! as HTMLImageElement).src,
+            badge: (promo.querySelector("br")!.previousSibling!.previousSibling as HTMLImageElement)!.src,
+          }
+
+          formattedPromos.push(formattedPromo)
+        })
+
+        return formattedPromos
+      })
+
+    await browser.close()
+
+    return NextResponse.json({data: promosObjects})
+  } catch (error) {
+    console.log(error)
+
+    return NextResponse.json({data: []});
+  }  
 }
 
 export const revalidate = 60
